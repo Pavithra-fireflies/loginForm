3ddc514e34f944acea37a0e032a9974a3e1acbaa
Pavithra Meganathan <66105765+Pavithra-fireflies@users.noreply.github.com>
2020-07-10 11:17:08 +0530

    Update DemoprojectApplicationTests.java

--- src/test/java/com/example/demo/DemoprojectApplicationTests.java
+++ src/test/java/com/example/demo/DemoprojectApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.context.annotation.ComponentScan;
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ComponentScan(basePackages={"com.example"})
+
 public class DemoprojectApplicationTests {
 
 	@Test
